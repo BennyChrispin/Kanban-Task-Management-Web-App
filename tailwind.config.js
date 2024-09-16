1dd32cad7ea86e0510f6aaca8a501cd1b0972949
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-09-16 15:37:17 +0200

    implement colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#635FC7",
+        primaryLight: "#A8A4FF",
+        black: "#000112",
+        darkGray: "#20212C",
+        mediumGray: "#2B2C37",
+        lightGray: "#3E3F4E",
+        softGray: "#828FA3",
+        lightBlue: "#E4EBFA",
+        paleBlue: "#F4F7FD",
+        white: "#FFFFFF",
+        red: "#EA5555",
+        lightRed: "#FF9898",
+      },
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+      },
+      fontWeight: {
+        medium: "500",
+        bold: "700",
+        extraBold: "800",
+      },
+    },
   },
   plugins: [],
 };
